f46320d4c63b0743e45e547fdd12a4f728605457
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-01-28 22:03:36 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -1,28 +1,31 @@
-<?php
+<?php 
+    $conn = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234") or die (mysqli_error($conn));
+    $db = mysqli_select_db($conn, "db_1820359");
+    if(mysqli_connect_errno($conn)){
+        echo "Database connection failed!: ". mysqli_connect_errno();
+    }
 
-$rss= '<?xml version="1.0" encoding="UTF-8"?>';
-$rss .= '<rss version="2.0">';
-$rss .= '<channel>';
+    $sql = "SELECT * FROM tbl_movies ORDER BY mov_id DESC LIMIT 20";
+    $q = mysqli_query($conn, $sql);
 
-$connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820359") or die (mysqli_error($connect));
-$sql = "SELECT * FROM tbl_movies";
-$query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
+    header("Content-type: text/xml");
 
-while($record= mysqli_fetch_assoc($query)) {
-	extract($record);
-	
-	$rss .= '<movie>';
-	$rss .= '<id>' . $mov_id .'</id>';
-	$rss .= '<title>' . $mov_title .'</title>';
-	$rss .= '<actor>' . $mov_actor .'</actor>';
-	$rss .= '<genre>' . $mov_genre .'</genre>';
-	$rss .= '<director>' . $mov_director .'</director>';
-	$rss .= '</movie>';
-}
+    echo "<?xml version='1.0' encoding='UTF-8'?>
+        <rss version='2.0'><channel>";
+    
+    while($r = mysqli_fetch_array($q)){
+        $title = $r['mov_title'];
+        $actor = $r['mov_actor'];
+        $genre = $r['mov_genre'];
+        $director = $r['mov_director'];
 
-$rss .= '</channel>';
-$rss .= '</rss>';
 
-echo$rss;
-?>	
-	
+        echo "<movie>
+        <title>$title</title>
+        <actor>$actor</actor>
+        <genre>$genre</genre>
+        <director>$director</director>
+        </movie>";
+    }
+    echo "</channel></rss>";
+?>
