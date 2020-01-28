af584ec27f760aa0c0131bb63156a11af51ef35f
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-01-28 22:09:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -32,3 +32,57 @@
 	<?php
 		}
 ?>
+<style>
+
+    .content{
+        display: flex;
+        justify-content: center;
+    }
+    .wrap{
+        display: flex;
+        flex-direction: column;
+        border: 1px solid black;
+        width: 30rem;
+    }
+    .border{
+        border: 1px solid black;
+        max-width: 40rem;
+    }
+</style>
+
+
+	
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://xmlexcercise1.herokuapp.com/rss.php");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("pet");
+?>
+ <div class="content">
+ <h1>movie</h1>
+ </div>
+ <div class="content">
+
+ <div class="wrap">
+ <?php
+ foreach( $content as $data )
+ {?>
+     <div class="border">
+     <?php
+     $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
+     $actor = $data->getElementsByTagName("actor")->item(0)->nodeValue;
+     $genre = $data->getElementsByTagName("genre")->item(0)->nodeValue;
+     $director = $data->getElementsByTagName("director")->item(0)->nodeValue;
+     echo "<ul><h2>$title - $actor</h2>
+              <ul>
+                  <li>genre: $genre </li>
+                  <li>director: $director </li>
+              </ul>
+          </ul>";
+    ?>
+     </div>
+  <?php
+ }
+?>
+</div>
+</div>
