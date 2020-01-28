ce7d1591573813d08ff1175044a049faccdd306d
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-01-28 11:44:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
 		foreach($content as $data)
 		{
 			
-			$mov_title = $data->getElementsbyTagName("movie title")->item(0)->nodeValue;
-			$mov_actor = $data->getElementsbyTagName("movie actor")->item(0)->nodeValue;
-			$mov_genre = $data->getElementsbyTagName("movie genre")->item(0)->nodeValue;
-			$mov_director = $data->getElementsbyTagName("movie director")->item(0)->nodeValue;
+			$mov_title = $data->getElementsbyTagName("title")->item(0)->nodeValue;
+			$mov_actor = $data->getElementsbyTagName("actor")->item(0)->nodeValue;
+			$mov_genre = $data->getElementsbyTagName("genre")->item(0)->nodeValue;
+			$mov_director = $data->getElementsbyTagName("director")->item(0)->nodeValue;
 			
 			
 			echo"<li>Movie:$movie
